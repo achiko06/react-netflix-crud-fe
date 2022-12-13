7862868590baaa77d5d1ddd89bef0153c34adcb0
achiko06 <achiko06@gmail.com>
2022-12-13 23:12:08 +0100

    routes basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,24 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/movies" element={<Home type="movie"/>}/>
+        <Route path="/series" element={<Home type="series"/>}/>
+        <Route path="/watch" element={<Watch />}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
